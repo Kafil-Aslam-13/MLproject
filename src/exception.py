3f898bf912ce30e4a14a0669bec79447f457f087
Kafil Aslam <aslamkafil13@gmail.com>
2025-07-01 13:42:50 +0530

    eda and problem statement understanding

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -21,10 +22,10 @@ class CustomException(Exception):
         return self.error_message
 
 
-# if __name__=="__main__":
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("logging divide by 0 has started")
-#         raise CustomException(e,sys)
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("logging divide by 0 has started")
+        raise CustomException(e,sys)
  #  above lines used only for checking       
