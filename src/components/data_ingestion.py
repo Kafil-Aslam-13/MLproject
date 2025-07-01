e23ab676e20eb33eedcf858e97d5056edace3d91
Kafil Aslam <aslamkafil13@gmail.com>
2025-07-01 21:25:37 +0530

    Data transformation is added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation ,DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -20,7 +21,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("entered data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv(r'notebook\data\stud.csv')
             logging.info("exported / read dataset as df")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -45,4 +46,7 @@ class DataIngestion:
             raise CustomException(e,sys)
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
